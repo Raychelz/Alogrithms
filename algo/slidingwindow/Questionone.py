781aaf43479ce67862f69205b7fb233e0b4b0818
Raychelz <mutegirachel@outlook.com>
2021-07-05 18:48:10 +0300

    added sub array
    added subarray add

--- algo/slidingwindow/Questionone.py
+++ algo/slidingwindow/Questionone.py
@@ -16,4 +16,27 @@ def find_average(a,n):
             print(Sum)
     return result
 
-print(find_average([1, 3, 2, 6, -1, 4, 1, 8, 2],5))
+#print(find_average([1, 3, 2, 6, -1, 4, 1, 8, 2],5))
+
+#Maximum Sum Subarray of Size K (easy)
+def subarraysum(a,k):
+    sum = 0.0
+    start =0
+    maxsub =0
+    for i in range(len(a)):
+        sum +=a[i]
+        #print(i)
+       # print(sum)
+        if i >= k -1:
+            maxsub = max(maxsub,sum)
+           # result.append(sum)
+            # if maxsub < sum:
+            #     maxsub=sum
+           #set sum to be less the first digit
+            sum -= a[start]
+            start+=1
+            #print(j)
+    return maxsub
+print(subarraysum([2, 1, 5, 1, 3, 2],3 ))
+
+print(subarraysum([2, 3, 4, 1, 5], 2 ))
